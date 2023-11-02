5c3b9bb93b5a42c0e12fb92b388394dd817bcb76
Eric Ming <ericmingyt@gmail.com>
2023-11-02 13:45:23 -0400

    Initial Commit

---
+++ mock-app/mock_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mock_appApp.swift
+//  mock-app
+//
+//  Created by Eric Ming on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct mock_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
